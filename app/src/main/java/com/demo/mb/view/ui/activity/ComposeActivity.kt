3777aadfaed65bda7b561aad1a36881dfce0aecc
慕绊 <mubanfive@qq.com>
2021-03-12 16:14:09 +0800

    修改gradle依赖，引入compose

--- app/src/main/java/com/demo/mb/view/ui/activity/ComposeActivity.kt
+++ app/src/main/java/com/demo/mb/view/ui/activity/ComposeActivity.kt
@@ -1,9 +1,11 @@
 package com.demo.mb.view.ui.activity
 
+import androidx.activity.compose.setContent
 import androidx.lifecycle.ViewModelProvider
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.demo.mb.R
 import com.demo.mb.databinding.ActivityComposeBinding
+import com.demo.mb.view.ui.screen.ComposeHomeScreen
 import com.demo.mb.viewmodel.DemoViewModel
 import com.muban.common.base.BaseActivity
 import com.muban.common.router.RouterPaths
@@ -18,6 +20,9 @@ class ComposeActivity : BaseActivity<DemoViewModel, ActivityComposeBinding>() {
     }
 
     override fun initView() {
+        setContent {
+            ComposeHomeScreen()
+        }
     }
 
     override fun initEvent() {
---
+++ app/src/main/java/com/demo/mb/view/ui/screen/ComposeHomeScreen.kt
@@ -0,0 +1,32 @@
+package com.demo.mb.view.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ComposeHomeScreen() {
+    Hello()
+}
+
+@Preview
+@Composable
+fun Hello() {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Compose")
+        Spacer(modifier = Modifier.size(10.dp))
+        Text("Hello Compose")
+    }
+}
